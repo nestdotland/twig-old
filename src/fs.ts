4e5328f7d486759507d342edb8f5c2e39df84752
divy-work <dj.srivastava23@gmail.com>
2020-07-11 16:26:02 +0530

    feat: use tmp in api-next folder

--- src/fs.ts
+++ src/fs.ts
@@ -2,11 +2,11 @@ import fs from "fs";
 import path from "path";
 
 export function init(time = 60, age = 1800) {
-  if (!fs.existsSync("./.tmp")) fs.mkdirSync("./.tmp");
+  if (!fs.existsSync("../../api-next/.tmp")) fs.mkdirSync("../../api-next/.tmp");
 
   const _interval = () => {
-    fs.readdirSync("./.tmp").map(el => {
-      let f = path.join("./.tmp", el);
+    fs.readdirSync("../../api-next/.tmp").map(el => {
+      let f = path.join("../../api-next/.tmp", el);
       let stats = fs.statSync(f);
 
       if (Date.now() - stats.mtimeMs > age * 1000) return fs.unlinkSync(f);
@@ -18,17 +18,17 @@ export function init(time = 60, age = 1800) {
 }
 
 export function save(id: string, data: Buffer) {
-  fs.writeFileSync(path.join("./.tmp", id), data);
+  fs.writeFileSync(path.join("../../api-next/.tmp", id), data);
 }
 
 export function get(id: string) {
-  let f = path.join("./.tmp", id);
+  let f = path.join("../../api-next/.tmp", id);
   if (fs.existsSync(f)) {
     return fs.readFileSync(f);
   } else return null;
 }
 
 export function has(id: string) {
-  let f = path.join("./.tmp", id);
+  let f = path.join("../../api-next/.tmp", id);
   return fs.existsSync(f);
 }
