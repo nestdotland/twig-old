fe8fb0318fc00dee18ce6982f2404a224b5bad34
divy-work <dj.srivastava23@gmail.com>
2020-08-03 14:30:25 +0530

    fix: typerror and fmt

--- src/arweave.ts
+++ src/arweave.ts
@@ -97,42 +97,45 @@ export async function save(
   transaction.addTag("Content-Type", data.type);
 
   await connection.transactions.sign(transaction, wallet || Credentials);
-  
-  const jwkWallet = await connection.wallets.jwkToAddress(wallet || Credentials);
+
+  const jwkWallet = await connection.wallets.jwkToAddress(
+    wallet || Credentials,
+  );
   let bal = await connection.wallets.getBalance(jwkWallet);
   let balAR = await connection.ar.winstonToAr(bal);
   let byteSize = data.data.byteLength;
   let fee = await Big(getWinston(byteSize));
 
-  if (wallet && balAR < fee + pstTipAmount || Credentials && balAR < fee) 
+  if (wallet && balAR < fee + pstTipAmount || Credentials && balAR < fee) {
     throw new Error("Insufficient funds!");
+  }
 
   if (wallet) {
     let pstRecipient = await pstAllocation();
     let pstTransaction = await connection.createTransaction({
       target: pstRecipient,
-      quantity: connection.ar.arToWinston(pstTipAmount.toString())
+      quantity: connection.ar.arToWinston(pstTipAmount.toString()),
     }, wallet);
     await connection.transactions.sign(pstTransaction, wallet);
     let pstRes = await connection.transactions.post(pstTransaction);
-    if (pstRes.status >= 300)
+    if (pstRes.status >= 300) {
       throw new Error("PST Tipping Transaction failed!");
+    }
   }
 
   const res = await connection.transactions.post(transaction);
 
-  if (res.status >= 300)
+  if (res.status >= 300) {
     throw new Error("Transaction failed!");
+  }
 
   return transaction.id;
 }
-
 
 /**
  * Credit to Anish Agnihotri's 'Weve' for the following code
  * https://github.com/Anish-Agnihotri/weve/blob/master/src/utils/pst.js
  */
-
 
 /**
  * Returns the wallet address of the user to send the tip to
@@ -140,7 +143,7 @@ export async function save(
 export const pstAllocation = async () => {
   let maxPST = 1000000000000;
   return calculateFeeRecipient(await getWalletList(), maxPST);
-}
+};
 
 /**
  * Returns the wallet list of PST holders
@@ -148,7 +151,7 @@ export const pstAllocation = async () => {
 export const getWalletList = async () => {
   let tipTX = await findContractTip(pstContract);
   return JSON.parse(await getTXState(tipTX)).walletList;
-}
+};
 
 /**
  * Calculates the recepient of the PST fee based on weighted randoms
@@ -159,35 +162,34 @@ const calculateFeeRecipient = (stakeholders, maxPST) => {
   let weightedStakeholders = {};
 
   for (let i = 0; i < stakeholders.length; i++) {
-    weightedStakeholders[stakeholders[i].addr] = stakeholders[i].balance / maxPST;
+    weightedStakeholders[stakeholders[i].addr] = stakeholders[i].balance /
+      maxPST;
   }
 
   return weightedRandom(weightedStakeholders);
-}
+};
 
 /**
  * @param probability The probability of the stakeholders receiving the tip
  */
-const weightedRandom = probability => {
+const weightedRandom = (probability) => {
   let i, sum = 0, r = Math.random();
 
   for (i in probability) {
     sum += probability[i];
     if (r <= sum) return i;
   }
-}
-
+};
 
 /**
  * Helper functions from SmartWeave
  */
 
-
 /**
  * Finds the latest contract tip
  * @param contractID The ID of a given PST smart contract
  */
-const findContractTip = async contractID => {
+const findContractTip = async (contractID) => {
   const contract = await getContract(contractID);
   let current = contract.contractTX;
   let state = getTXState(current);
@@ -195,10 +197,9 @@ const findContractTip = async contractID => {
 
   do {
     last = current;
-    current = await findNextTX(contract, state, current);
+    current = await findNextTX(contract, state, current) || current;
     state = getTXState(current);
-  }
-  while (current);
+  } while (current);
 
   return last;
 };
@@ -207,55 +208,56 @@ const findContractTip = async contractID => {
  * Returns information about a PST smart contract
  * @param contractID The ID of a given PST smart contract
  */
-const getContract = async contractID => {
+const getContract = async (contractID) => {
   const contractTX = await arweaveInit.transactions.get(contractID);
-  const contractSrcTXID = await getTag(contractTX, 'Contract-Src');
-  const minDiff = await getTag(contractTX, 'Min-Diff');
+  const contractSrcTXID = await getTag(contractTX, "Contract-Src");
+  const minDiff = await getTag(contractTX, "Min-Diff");
   const contractSrcTX = await arweaveInit.transactions.get(contractSrcTXID);
-  const contractSrc = await contractSrcTX.get('data', { decode: true, string: true });
-  const state = await contractTX.get('data', { decode: true, string: true });
+  const contractSrc = await contractSrcTX.get(
+    "data",
+    { decode: true, string: true },
+  );
+  const state = await contractTX.get("data", { decode: true, string: true });
 
   return {
     id: contractID,
     contractSrc: contractSrc,
     initState: state,
     minDiff: minDiff,
-    contractTX: contractTX
-  }
-}
+    contractTX: contractTX,
+  };
+};
 
 /**
  * Finds the next transaction
  */
 const findNextTX = async (contract, state, currentTX) => {
-  let successorsQuery =
-  {
-    op: 'and',
-    expr1:
-    {
-      op: 'equals',
-      expr1: 'App-Name',
-      expr2: 'nest.land'
+  let successorsQuery = {
+    op: "and",
+    expr1: {
+      op: "equals",
+      expr1: "App-Name",
+      expr2: "nest.land",
     },
-    expr2:
-    {
-      op: 'equals',
-      expr1: 'Previous-TX',
-      expr2: currentTX.id
-    }
-  }
-  const response = await arweaveInit.api.post(`arql`, successorsQuery)
-  const results = response.data
+    expr2: {
+      op: "equals",
+      expr1: "Previous-TX",
+      expr2: currentTX.id,
+    },
+  };
+  const response = await arweaveInit.api.post(`arql`, successorsQuery);
+  const results = response.data;
 
-  let successors = (results == '') ? [] : results
+  let successors = (results == "") ? [] : results;
 
   for (let i = 0; i < successors.length; i++) {
-    let TX = await arweaveInit.transactions.get(successors[i])
-    if (validateNextTX(contract, state, TX))
-      return TX
+    let TX = await arweaveInit.transactions.get(successors[i]);
+    if (validateNextTX(contract, state, TX)) {
+      return TX;
+    }
   }
 
-  return false
+  return false;
 };
 
 /**
@@ -263,13 +265,15 @@ const findNextTX = async (contract, state, currentTX) => {
  * @param TX A transaction ID
  */
 const getTXState = async (TX) => {
-  if (!TX) return false
-  if (await getTag(TX, 'Type') == "contract")
-    return TX.get('data', { decode: true, string: true })
-  else
-    return JSON.parse(TX.get('data', { decode: true, string: true }))['newState']
+  if (!TX) return false;
+  if (await getTag(TX, "Type") == "contract") {
+    return TX.get("data", { decode: true, string: true });
+  } else {
+    return JSON.parse(
+      TX.get("data", { decode: true, string: true }),
+    )["newState"];
+  }
 };
-
 
 /**
  * Returns a boolean signifying whether a transaction has tag 'name'
@@ -277,41 +281,41 @@ const getTXState = async (TX) => {
  * @param name The name of a desired tag
  */
 const getTag = async (TX, name) => {
-  let tags = TX.get('tags')
+  let tags = TX.get("tags");
 
-  for (let i = 0; i < tags.length; i++)
-    if (tags[i].get('name', { decode: true, string: true }) == name)
-      return tags[i].get('value', { decode: true, string: true })
+  for (let i = 0; i < tags.length; i++) {
+    if (tags[i].get("name", { decode: true, string: true }) == name) {
+      return tags[i].get("value", { decode: true, string: true });
+    }
+  }
 
-  return false
+  return false;
 };
 
 /**
  * Returns data about validating the next transaction
  */
 const validateNextTX = async (contract, state, nextTX) => {
-  let struct = JSON.parse(nextTX.get('data', { decode: true, string: true }))
+  let struct = JSON.parse(nextTX.get("data", { decode: true, string: true }));
   return (
     contract.contractSrc,
-    struct.input,
-    state,
-    await arweaveInit.wallets.ownerToAddress(nextTX.owner)
+      struct.input,
+      state,
+      await arweaveInit.wallets.ownerToAddress(nextTX.owner)
   );
 };
-
 
 /**
  * Credit to Sergej Müller's 'chaiku' for the following code
  * https://github.com/sergejmueller/chaiku/blob/master/js/main.js
  */
 
-
 /**
  * Returns the fee to upload x bytes to y target
  */
 const getWinston = async (bytes?, target?) => {
   bytes = bytes || 0;
-  target = target || '';
+  target = target || "";
 
   const response = await fetch(`https://arweave.net/price/${bytes}/${target}`);
 
